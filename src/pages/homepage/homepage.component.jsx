5e24744739977680fe0e9d8cd261f536d0cfdde5
David Wu <dwu97@yahoo.com>
2020-04-04 15:28:04 -0400

    Add basic header

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -2,11 +2,8 @@ import React from "react";
 
 import Directory from "../../components/directory-menu/directory-menu.component";
 
-import {Link} from "react-router-dom";
-
 const HomePage = () => (
   <div className="homepage">
-    <Link to="shop"><h1>Shop</h1></Link>
     <Directory />
   </div>
 )
